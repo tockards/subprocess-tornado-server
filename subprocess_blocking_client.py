a964e8ae5800dcc1a3cff96027214f721b269819
Toufeeq Ockards <tockards@gmail.com>
2016-06-30 20:40:54 +0200

    using socketlib

--- subprocess_blocking_client.py
+++ subprocess_blocking_client.py
@@ -11,7 +11,24 @@ This server uses telnetlib to access the subprocess server
 """
 import telnetlib
 from sys import argv
+import socket
 
+
+def main2():
+    """
+    """
+
+    con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    con.connect((argv[1], int(argv[2])))
+    args = ' '.join(argv[3:])+ '\n'
+    con.send(args)
+    lines = ''
+    while True:
+       data = con.recv(102400)
+       if not data:
+          break
+       lines = lines + data
+    print(lines)
 
 def main():
     """TODO: Docstring for main.
@@ -33,5 +50,5 @@ def main():
             break
 
 if __name__ == "__main__":
-    main()
+    main2()
 
